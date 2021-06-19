125b23ba02b0df9fa2a0308d1085f9aff932a959
the-injineer <jobinb6444@outlook.com>
2021-06-19 14:41:19 +0530

    Updated Contact

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,20 @@
+import styles from "../styles/ContactPage.module.css";
+import Link from "next/link";
+
 const ContactPage = () => {
   return (
     <>
       <h1>Contact</h1>
+      <div className={styles.container}>
+      <div className={styles.buttons}>
+          <Link href="https://www.instagram.com/the_injineer">
+            <button className={styles.contained}>Instagram</button>
+          </Link>
+          <Link href="mailto:jobinb6444@gmail.com">
+            <button className={styles.outlined}>Email Me</button>
+          </Link>
+      </div>
+      </div>
     </>
   );
 };
